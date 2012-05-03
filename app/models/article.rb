f35ab6ef17787c9665638e87f8f32185a9588a35
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2012-05-03 19:51:03 -0400

    added scope for articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,11 @@
 class Article < ActiveRecord::Base
+
   belongs_to :category
+
+  validate_presence_of :title
+  validate_presence_of :content
+
+  scope :alphabetical, order('title')
+  scope :active, where('active = 1')
+
 end
