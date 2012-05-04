f81355475716a749a1de4f1d250b0c2120d0626e
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2012-05-03 20:05:52 -0400

    corrected article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,8 +2,8 @@ class Article < ActiveRecord::Base
 
   belongs_to :category
 
-  validate_presence_of :title
-  validate_presence_of :content
+  validates_presence_of :title
+  validates_presence_of :content
 
   scope :alphabetical, order('title')
   scope :active, where('active = 1')
